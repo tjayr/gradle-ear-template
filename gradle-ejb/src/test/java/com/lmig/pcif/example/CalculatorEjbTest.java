e0a4725308391d2c666098d57c7a68ad62913cc3
Tony Ayres <tayres@gmail.com>
2015-03-15 22:19:32 +0000

    Switch to Eclipse link JPA

--- gradle-ejb/src/test/java/com/lmig/pcif/example/CalculatorEjbTest.java
+++ gradle-ejb/src/test/java/com/lmig/pcif/example/CalculatorEjbTest.java
@@ -2,15 +2,9 @@ package com.lmig.pcif.example;
 
 import static org.junit.Assert.assertEquals;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
-import org.hsqldb.Server;
-import org.hsqldb.persist.HsqlProperties;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.persistence.DataSource;
-import org.jboss.arquillian.persistence.ShouldMatchDataSet;
-import org.jboss.arquillian.persistence.UsingDataSet;
+import org.jboss.arquillian.persistence.*;
 import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -18,63 +12,39 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
-import javax.naming.NamingException;
 import java.io.File;
 
 /**
  * CalculatorTest.
  */
 @RunWith(Arquillian.class)
+@CreateSchema("create_useradd.sql")
+@Cleanup(phase = TestExecutionPhase.NONE)
 public class CalculatorEjbTest {
-
-    private static org.apache.log4j.Logger log = Logger.getLogger(CalculatorEjb.class);
-
-    private Server server;
 
     @EJB
     private CalculatorInterface calculator;
 
     @Deployment(name = "CalculatorEjbTest")
     public static Archive<?> createDeployment() {
-        log.info("create deployment");
+        System.out.println("create deployment");
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "ejb.jar")
                 .addClasses(CalculatorEjb.class, CalculatorEjbTest.class, CalculatorInterface.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAsManifestResource("ibm-ejb-bnd.xml")
                 .addAsManifestResource("was-persistence/persistence.xml", "persistence.xml");
 
-        //File[] deps = Maven.resolver().resolve("org.hsqldb:hsqldb:2.3.1").withTransitivity().asFile();
-
-        File[] deps = Maven.resolver().resolve("org.hibernate:hibernate-entitymanager:3.6.7.Final").withTransitivity().asFile();
-
+        File[] deps = Maven.resolver().resolve("org.eclipse.persistence:eclipselink:2.5.0").withTransitivity().asFile();
+        //File[] deps = Maven.resolver().resolve("org.hibernate:hibernate-entitymanager:3.6.7.Final").withTransitivity().asFile();
 
         EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, "calculator.ear");
         ear.addAsModule(jar);
         ear.addAsLibraries(deps);
         return ear;
-    }
-
-
-    public void getDS() {
-        javax.naming.InitialContext ctx = null;
-        javax.sql.DataSource ds = null;
-        log.info("Attempting connection...");
-        System.out.println("connect");
-        try {
-            ctx = new javax.naming.InitialContext();
-            ds = (javax.sql.DataSource) ctx.lookup("jdbc/derby");
-            log.info(ds.toString());
-            System.out.println(ds.toString());
-        } catch (NamingException e) {
-            System.out.println(e) ;
-            log.error("peformanceappraisalstatus: COULDN'T CREATE CONNECTION!");
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -86,9 +56,7 @@ public class CalculatorEjbTest {
     @ShouldMatchDataSet("datasets/dummy-dataset.xml")
     @Transactional
     public void testAdd() {
-        log.debug("testAdd");
         System.out.println("testAdd");
-        getDS();
         int result = calculator.add(new int[]{1, 2, 3, 4});
         assertEquals(10, result);
     }
